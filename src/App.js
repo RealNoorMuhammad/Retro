58d0c5719a23ef8db91de05c3fb687a9cc2e9f5e
Noor Muhammad <95448023+RealNoorMuhammad@users.noreply.github.com>
2022-09-23 00:19:37 +0500

    Reto

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="bg">
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <br />
+      <br />
+      <br />
+
+      <br />
+
+      <h1
+        style={{
+          color: "#fff",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          fontSize: "80px",
+        }}
+      ></h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/ComingSoon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import soon from './coming.jpg'
+
+const ComingSoon = () => {
+  return (
+    <div className='bg'>ComingSoon</div>
+  )
+}
+
+export default ComingSoon
